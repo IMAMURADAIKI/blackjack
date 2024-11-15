c39fb536b21163103fc8c2860afc35592294f44b
IMAMURADAIKI <i.daiki2102@gmail.com>
2024-11-15 10:28:27 +0900

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -57,7 +57,7 @@ class BlackjackGame {
 
     addCardToHand(card, hand, index = 0) {
         const cardImage = document.createElement('img');
-        cardImage.src = `github.io/cards/${card.value}_${card.suit}.png`;
+        cardImage.src = `https://imamuradaiki.github.io/github.io/cards/${card.value}_${card.suit}.png`;
         cardImage.classList.add('card-image');
         cardImage.style.animationDelay = `${index * 0.2}s`;
         cardImage.classList.add('deal-animation');
@@ -94,7 +94,7 @@ class BlackjackGame {
         this.addCardToHand(this.dealerHand[0], 'dealer', 0);
                 
         this.dealerHiddenCardElement = document.createElement('img');
-        this.dealerHiddenCardElement.src = '../cards/back.png';
+        this.dealerHiddenCardElement.src = 'https://imamuradaiki.github.io/github.io/cards/back.png';
         this.dealerHiddenCardElement.classList.add('card-image', 'hidden-card');
         document.getElementById('dealer-cards').appendChild(this.dealerHiddenCardElement);
                 
