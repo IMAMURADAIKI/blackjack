71585212f1c41229816adb2c5709ace476b24dd5
IMAMURADAIKI <i.daiki2102@gmail.com>
2025-01-24 15:16:07 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,6 +28,7 @@ let insuranceAvailable = false;
 
 sound0.pause();
 sound0.currentTime = 0;
+sound0.volume = 0.7;
 sound0.play();
 // スタート
 document.getElementById('deal_b').addEventListener('click', () => {
